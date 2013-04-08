729164b0d3707382fab635ad8af8dfc2979f9f38
shadowbq <shadowbq@gmail.com>
2013-04-08 14:37:05 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,9 +3,13 @@ Redmine::Plugin.register :redmine_global_issues do
   author 'shadowbq'
   description 'Shows a link on the top menu to link to all issues, and the calendar'
 
+  url 'http://github.com/shadowbq/redmine_global_issues'
+  author_url 'mailto:shadowbq@gmail.com'
+
+  requires_redmine :version_or_higher => '2.0.0'
   version '0.1.0'
   
-  menu :top_menu, :label_issue_plural, "/issues"
-  menu :top_menu, :label_calendar, "/issues/calendar"
+  menu :top_menu, :issues, :caption => :label_issue_plural, "/issues"
+  menu :top_menu, :calendar, :caption => :label_calendar, "/issues/calendar"
 end
 
